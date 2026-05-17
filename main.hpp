b7f11e65541b05c4c8c5336d106c4f79c5b4c7c1
d4niel <dhaddad3@students.avc.edu>
2026-05-16 19:30:32 -0700

    commit 1

--- main.hpp
+++ main.hpp
@@ -1,22 +1,43 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
+
 // Count cross-shapes in a 10x10 binary grid.
 // A cross is centered at M[i][j]==1 with M[i-1][j], M[i+1][j],
 // M[i][j-1], M[i][j+1] all equal to 1.
+
 
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
+
 int findcross(int[][10]);
 void printout(int[][10], int);
 
+
 int findcross(int M[][10])
 {
-    // TODO: scan every cell. If M[i][j]==1 and all four orthogonal neighbors
-    //       are also 1, count it as a cross. Return the total count.
+    int cnt = 0;
+
+    for (int i = 1; i < 9; ++i)
+    {
+        for (int j = 1; j < 9; ++j)
+        {
+            if (M[i][j] == 1 &&
+                M[i - 1][j] == 1 &&
+                M[i + 1][j] == 1 &&
+                M[i][j - 1] == 1 &&
+                M[i][j + 1] == 1)
+            {
+                cnt++;
+            }
+        }
+    }
+
+    return cnt;
 }
+
 
 void printout(int M[][10], int cnt)
 {
@@ -26,7 +47,8 @@ void printout(int M[][10], int cnt)
             cout << setw(5) << M[i][j];
         cout << endl;
     }
-    cout << "Total found cross " << cnt << endl;
+    cout << "Total found cross "
 }
+
 
 #endif
